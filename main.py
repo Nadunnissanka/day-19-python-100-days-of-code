2d60e51e34031de7dd1c3f25be066782b92f33b4
Nadun Nissanka <61065783+Nadunnissanka@users.noreply.github.com>
2021-06-22 22:55:15 +0530

    sketch program changes in python

--- main.py
+++ main.py
@@ -8,6 +8,21 @@ def move_forward():
     turtle.fd(10)
 
 
+def move_backward():
+    turtle.bk(10)
+
+
+def move_left():
+    turtle.lt(10)
+
+
+def move_right():
+    turtle.rt(10)
+
+
 screen.listen()
-screen.onkey(key="space", fun=move_forward)
+screen.onkey(key="w", fun=move_forward)
+screen.onkey(key="s", fun=move_backward)
+screen.onkey(key="a", fun=move_left)
+screen.onkey(key="d", fun=move_right)
 screen.exitonclick()
